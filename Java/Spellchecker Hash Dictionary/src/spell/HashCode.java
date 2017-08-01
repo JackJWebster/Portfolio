8bad1397ad05c4531f5b1a8d2cca7b65533bb717
Jack Webster <jackwebby95@gmail.com>
2017-08-01 17:10:32 +0100

    Removed Duplicates

--- Java/Spellchecker Hash Dictionary/src/spell/HashCode.java
+++
@@ -1,8 +0,0 @@
-package spell;
-
-//Interface for HashCode. Has only one method declaration
-public interface HashCode {
-
-	// For an input object, computes and returns its HashCode as an integer
- public int giveCode(Object o);
-}
